4cadd782f3257641046f7bc883ac18115402caed
angelo <angelo.garzia@hotmail.fr>
2025-12-09 11:49:35 +0100

    tp1-2

--- src/test/java/java17/ex02/Method_02_Test.java
+++ src/test/java/java17/ex02/Method_02_Test.java
@@ -41,7 +41,7 @@ public class Method_02_Test {
         // TODO l'implémentation réutilise la méthode format() de l'interface
         @Override
         public String format(){
-            return "DaoA[" + findAll().size() + " persons]";
+            return "DaoA" + IDao.super.format();
         }
 
     }
