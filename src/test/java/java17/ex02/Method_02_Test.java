9a5a2619eabf16144c9b84def29dc61d3ae9f1cf
angelo <angelo.garzia@hotmail.fr>
2025-12-09 11:53:49 +0100

    tp1-2

--- src/test/java/java17/ex02/Method_02_Test.java
+++ src/test/java/java17/ex02/Method_02_Test.java
@@ -41,7 +41,7 @@ public class Method_02_Test {
         // TODO l'implémentation réutilise la méthode format() de l'interface
         @Override
         public String format(){
-            return "DaoA" + IDao.super.format();
+            return this.getClass().getSimpleName() + IDao.super.format();
         }
 
     }
